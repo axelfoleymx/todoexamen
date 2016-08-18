1e34dfa4a3e25a7b5b6fd0b6002d493f08f84970
¨axelfoleymx¨ <a01164173@gmail.com>
2016-08-17 21:30:51 -0500

    Agregue un test en student

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+def setup
+  @student = student(:one)
+end
+
+test 'invalid withot a name' do
+  skip " This test I'll skip"
+  @student.name = nil
+  asset !@student.valid?, 'name is not being validated'
+end
+
 end
